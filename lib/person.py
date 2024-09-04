efa56e213221fd1f058aec2241743527d07693ae
terenceouma <doros.devs@gmail.com>
2024-09-04 11:22:48 +0300

    adds instances in person and dog file

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    # Define the talk instance method
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    # Define the walk instance method
+    def walk(self):
+        print("The person is walking.")
